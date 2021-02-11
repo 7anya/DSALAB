012e9ee6e680ef008d9b2a2a55660912e5dcfa72
Tanya Prasad <f20181054@hyderabad.bits-pilani.ac.in>
2021-02-11 21:13:08 +0530

    eeee

--- Assignment2/Question D.h
+++ Assignment2/Question D.h
@@ -9,3 +9,70 @@
 
 
 
+#include <stdio.h>
+#define N 100005
+#define mod 1000000007
+#define int long long
+
+int a[N], p[N], n, m;
+
+int power(int a, int b, int m)
+{
+    int res = 1;
+    a %= m;
+    while (b > 0)
+    {
+        if(b & 1)
+            res = res *a % m;
+        a = a *a % m;
+        b >>= 1;
+    }
+
+    return res;
+}
+
+int modinv(int k)
+{
+    return power(k, mod - 2, mod);
+}
+
+int right(int idx)
+{
+    if(idx >= m)
+        return m;
+    else
+        return idx;
+}
+
+int left(int idx)
+{
+    if(idx <= n - m + 1)
+        return 1;
+    else
+        return idx - (n - m);
+}
+
+void solveD()
+{
+    scanf("%d %d\n", &n, &m);
+    a[0] = 1;
+    p[0] = 1;
+    for(int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    for(int i = 1; i <= m; i++)
+        scanf("%d", &p[i]);
+    for(int i = 1; i <= m; i++)
+    {
+        p[i] = (p[i] *p[i - 1]) % mod;
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        a[i] = (((a[i] *p[right(i)]) % mod) *modinv(p[left(i) - 1])) % mod;
+        if(i > 1)
+            printf(" ");
+        printf("%d", a[i]);
+    }
+
+
+}
--- Assignment2/Question E.h
+++ Assignment2/Question E.h
@@ -9,50 +9,33 @@
 
 #endif //DSALAB_QUESTION_E_H
 
-void printAllKLengthRec(char set[], char prefix[],int n, int k)
+
+#define N 100005
+
+int n, k, flg = 0;
+char ans[N], s[N];
+
+void recur(int idx)
 {
-
-    // Base case: k is 0,
-    // print prefix
-    if (k == 0)
+    if(idx == k)
     {
-//        for (int i = strlen(prefix)-3; i <strlen(prefix) ; ++i) {
-//            printf("%c",prefix[i]);
-//        }
-
-        printf("%s\n",prefix);
-//        cout << (prefix) << endl;
+        if(flg) printf("\n");
+        flg = 1;
+        printf("%s", ans);
         return;
     }
-
-    // One by one add all characters
-    // from set and recursively
-    // call for k equals to k-1
-    for (int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-
-        prefix[strlen(prefix)]=set[i];
-        prefix[strlen(prefix)+1]='\0';
-
-        printAllKLengthRec(set,prefix, n, k - 1);
+        ans[idx] = s[i];
+        recur(idx + 1);
     }
-
 }
 
-void printAllKLength(char set[], int k,int n)
-{
-    char a[100];
-
-    printAllKLengthRec(set, a, n, k);
-}
-
-// Driver Code
 void solveE()
 {
-
-    char set1[] = {'a', 'b'};
-    int k = 3;
-    printAllKLength(set1, k, 2);
-
+    scanf("%d %d\n", &n, &k);
+    scanf("%s", s);
+    ans[k] = '\0';
+    recur(0);
 
 }
