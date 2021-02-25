3c1f76235d2e1069fbfbcc4213ae067c1dc9c506
Tanya Prasad <f20181054@hyderabad.bits-pilani.ac.in>
2021-02-25 18:32:19 +0530

    isaac asimov ftw

--- Assignment 4/Question B.h
+++ Assignment 4/Question B.h
@@ -10,11 +10,15 @@
 #define DSALAB_QUESTION_B_H
 
 #endif //DSALAB_QUESTION_B_H
-
-int replace(int a[], int n) {
-    int b[n], k = 0;
+struct element
+{
+    int index;
+    int val;
+};
+int replace(struct element a[], int n) {
+    struct element b[n];int  k = 0;
     for (int i = 0; i < n; ++i) {
-        if (a[i] == -1)
+        if (a[i].val == -1)
             continue;
         b[k++] = a[i];
     }
@@ -24,9 +28,9 @@ int replace(int a[], int n) {
     return k;
 }
 
-void print(int a[], int n) {
+void print(struct element a[], int n) {
     for (int i = 0; i < n; ++i) {
-        printf("%d ", a[i]);
+        printf("%d ", a[i].val);
     }
     printf("\n");
 }
@@ -34,45 +38,42 @@ void print(int a[], int n) {
 void solve() {
     int n, alpha, beta;
     scanf("%d", &n);
-    int a[n];
+    struct element a[n];
     scanf("%d", &alpha);
     scanf("%d", &beta);
     int m=INT_MIN,ind;
     for (int i = 0; i < n; ++i) {
-        scanf("%d", &a[i]);
-        if(a[i]>m)
-        {
-            m=a[i];
-            ind=i;
-        }
+
+        scanf("%d", &a[i].val);
+      a[i].index=i;
     }
 
     while (n > 1) {
         if (n % 2 == 1) {
-            a[n - 1] += beta;
+            a[n - 1].val += beta;
 //            print(a, n);
         }
         for (int i = 0; i < n - 1; i += 2) {
-            if (a[i] > a[i + 1]) {
-                a[i] = abs(a[i] - alpha * (a[i] - a[i + 1]));
-                a[i + 1] = -1;
+            if (a[i].val > a[i + 1].val) {
+                a[i].val = abs(a[i].val - alpha * (a[i].val - a[i + 1].val));
+                a[i + 1].val = -1;
 //                print(a, n);
-            } else if (a[i + 1] > a[i]) {
-                a[i + 1] = abs(a[i + 1] - alpha * (a[i + 1] - a[i]));
-                a[i] = -1;
+            } else if (a[i + 1].val > a[i].val) {
+                a[i + 1].val = abs(a[i + 1].val- alpha * (a[i + 1].val - a[i].val));
+                a[i].val = -1;
 //                print(a, n);
             } else {
-                a[i] = -1;
-                a[i + 1] = -1;
+                a[i].val = -1;
+                a[i + 1].val = -1;
 //                print(a, n);
             }
         }
         n = replace(a, n);
 //        print(a, n);
     }
-    if (a[0] == -1)
+    if (a[0].val == -1)
         printf("-1 -1");
     else
-        printf("%d %d",ind+1, a[0]);
+        printf("%d %d",a[0].index+1, a[0].val);
 
 }
